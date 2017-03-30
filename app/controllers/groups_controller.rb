20e24982c6902114e06c631a8542cbe26fc882a9
guyan <norekcn@gmail.com>
2017-03-30 10:47:26 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -34,6 +34,7 @@ def create
   @group = Group.new(group_params)
   @group.user = current_user
   if @group.save
+    current_user.join!(@group)
   redirect_to groups_path
 else
   render :new
@@ -60,7 +61,7 @@ def quit
     flash[:warning] = "你已经是本讨论版成员，怎么退出"
   end
   redirect_to group_path(@group)
-end 
+end
 
 private
 def find_group_and_check_permission
